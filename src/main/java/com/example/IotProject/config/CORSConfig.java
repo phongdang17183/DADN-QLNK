8fdf051b706fa9032baf2b8f90931c36469a5e8e
dlpduy <dinhlephucduy@gmail.com>
2025-03-06 20:34:43 +0700

    config CORS

---
+++ src/main/java/com/example/IotProject/config/CORSConfig.java
@@ -0,0 +1,30 @@
+package com.example.IotProject.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:port")); // Allow all origins
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE",
+                "OPTIONS")); // Allowed methods
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type",
+                "Accept"));
+        configuration.setAllowCredentials(true); // Allow credentials
+        configuration.setMaxAge(3600L);
+        // How long the response from a pre-flight request can be cached by clients
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply this configuration to all paths
+        return source;
+    }
+}
