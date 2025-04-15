99205e66905869ef38c73f3f8be2f9e960540fc6
Duy Phúc <dinhlephucduy@gmail.com>
2025-04-15 18:43:33 +0700

    adjust filed Fullname

--- src/main/java/com/example/IotProject/service/AuthService/EmailService.java
+++ src/main/java/com/example/IotProject/service/AuthService/EmailService.java
@@ -123,7 +123,7 @@ public class EmailService {
         sendEmail(email);
     }
 
-    public void sendMailArletTemperature(String fullName, String data, ConditionRuleResponse res, RuleModel rule) {
+    public void sendMailArletTemperature(String data, ConditionRuleResponse res, RuleModel rule) {
         String subject = "Cảnh báo nhiệt độ Bontanica";
         String body = """
             <!DOCTYPE html>
@@ -184,7 +184,7 @@ public class EmailService {
             <body>
             <div class="email-container">
                 <h2>
-                Kính gửi """ +" "+fullName + ","+"""
+                Kính gửi """ +" "+rule.getUser().getFullName() + ","+"""
                 </h2>
                 
                 <p>Hệ thống <strong>Bontanica</strong> vừa ghi nhận nhiệt độ tại khu vực trồng khoai lang của bạn đã vượt quá mức an toàn:</p>
@@ -230,7 +230,7 @@ public class EmailService {
         sendEmail(email);
     }
 
-    public void sendMailAlertLight(String fullName, String data, ConditionRuleResponse res, RuleModel rule) {
+    public void sendMailAlertLight(String data, ConditionRuleResponse res, RuleModel rule) {
         String subject = "Cảnh báo ánh sáng Bontanica";
         String body = """
             <!DOCTYPE html>
@@ -290,7 +290,7 @@ public class EmailService {
             </head>
             <body>
             <div class="email-container">
-                <h2>Kính gửi """ + fullName + "," + """
+                <h2>Kính gửi """ + rule.getUser().getFullName() + "," + """
                 </h2>
                 
                 <p>Hệ thống <strong>Bontanica</strong> vừa ghi nhận mức ánh sáng tại khu vực trồng khoai lang của bạn đã vượt quá mức an toàn:</p>
@@ -336,7 +336,7 @@ public class EmailService {
         sendEmail(email);
     }
     
-    public void sendMailAlertSoilMoisture(String fullName, String data, ConditionRuleResponse res, RuleModel rule) {
+    public void sendMailAlertSoilMoisture(String data, ConditionRuleResponse res, RuleModel rule) {
         String subject = "Cảnh báo độ ẩm đất Bontanica";
         String body = """
             <!DOCTYPE html>
@@ -396,7 +396,7 @@ public class EmailService {
             </head>
             <body>
             <div class="email-container">
-                <h2>Kính gửi """ + fullName + "," + """
+                <h2>Kính gửi """ + rule.getUser().getFullName() + "," + """
                 </h2>
                 
                 <p>Hệ thống <strong>Bontanica</strong> vừa ghi nhận độ ẩm đất tại khu vực trồng khoai lang của bạn đã vượt quá mức an toàn:</p>
@@ -442,7 +442,7 @@ public class EmailService {
         sendEmail(email);
     }
     
-    public void sendMailAlertHumidity(String fullName, String data, ConditionRuleResponse res, RuleModel rule) {
+    public void sendMailAlertHumidity(String data, ConditionRuleResponse res, RuleModel rule) {
         String subject = "Cảnh báo độ ẩm không khí Bontanica";
         String body = """
             <!DOCTYPE html>
@@ -502,7 +502,7 @@ public class EmailService {
             </head>
             <body>
             <div class="email-container">
-                <h2>Kính gửi """ + fullName + "," + """
+                <h2>Kính gửi """ + rule.getUser().getFullName() + "," + """
                 </h2>
                 
                 <p>Hệ thống <strong>Bontanica</strong> vừa ghi nhận độ ẩm không khí tại khu vực trồng khoai lang của bạn đã vượt quá mức an toàn:</p>
